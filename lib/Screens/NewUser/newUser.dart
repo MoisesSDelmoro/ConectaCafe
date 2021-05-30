9bc6b2d13cd91bbc66c1669c8fab7ce12e6fcbbb
moisessdelmoro <moisesdelmoro@gec.inatel.br>
2021-05-30 12:06:36 -0300

    New User

--- lib/Screens/NewUser/newUser.dart
+++ lib/Screens/NewUser/newUser.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_auth/Screens/Register/components/Body.dart';
 
-class RegisterScreen extends StatelessWidget {
+class NewUser extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
