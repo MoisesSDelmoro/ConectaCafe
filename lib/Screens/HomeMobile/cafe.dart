766a056791a149a466e2bf79eecbdd61dbf2d7cf
Leo18ss <lsstdm@live.com>
2021-06-02 11:21:22 -0300

    cafe_sceen

--- lib/Screens/HomeMobile/cafe.dart
+++ lib/Screens/HomeMobile/cafe.dart
@@ -24,7 +24,109 @@ class _CafeState extends State<Cafe> {
           child: Center(
             child: Column(
               mainAxisSize: MainAxisSize.min,
-              children: <Widget>[],
+              children: <Widget>[
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  width: 160,
+                  height: 200,
+                  margin: const EdgeInsets.all(12.0),
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 1,
+                        blurRadius: 5,
+                        offset: Offset(0, 0), // changes position of shadow
+                      ),
+                    ],
+                    color: Color.fromARGB(255, 250, 232, 187),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(15),
+                    ),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Foto 1',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 160,
+                  height: 200,
+                  margin: const EdgeInsets.all(12.0),
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 1,
+                        blurRadius: 5,
+                        offset: Offset(0, 0), // changes position of shadow
+                      ),
+                    ],
+                    color: Color.fromARGB(255, 250, 232, 187),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(15),
+                    ),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Foto 1',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 160,
+                  height: 200,
+                  margin: const EdgeInsets.all(12.0),
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 1,
+                        blurRadius: 5,
+                        offset: Offset(0, 0), // changes position of shadow
+                      ),
+                    ],
+                    color: Color.fromARGB(255, 250, 232, 187),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(15),
+                    ),
+                  ),
+                  child: Center(
+                    child: Column(
+                      children: [
+                        Text(
+                          'Cafe 1',
+                          style: TextStyle(fontSize: 22),
+                        ),
+                        Text(
+                          'Marca 1',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: Row(
+                            children: [
+                              Icon(Icons.edit),
+                              Text(
+                                "Editar",
+                                style: TextStyle(fontSize: 18),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ),
